6c274ac558fc4d2f31d3ce368deb5fc4f0b83c4c
brightmart <brightmart@hotmail.com>
2018-10-08 03:41:50 +0800

    Update config.py

--- magpie/config.py
+++ magpie/config.py
@@ -2,7 +2,7 @@
 EMBEDDING_SIZE = 100
 
 # Cores to use while fitting word2vec vectors
-WORD2VEC_WORKERS = 4
+WORD2VEC_WORKERS = 20 #4
 MIN_WORD_COUNT = 5
 WORD2VEC_CONTEXT = 5
 
@@ -10,8 +10,8 @@ WORD2VEC_CONTEXT = 5
 NN_ARCHITECTURE = 'cnn' # 
 
 # Training parameters
-BATCH_SIZE = 64
+BATCH_SIZE = 128 #64
 EPOCHS = 1
 
 # Number of tokens to save from the abstract, zero padded
-SAMPLE_LENGTH = 200
+SAMPLE_LENGTH = 300 #200
